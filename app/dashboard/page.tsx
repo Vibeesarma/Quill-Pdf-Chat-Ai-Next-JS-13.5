58873910762a11f84593e2cae1f4e119efd83600
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-10-23 17:14:58 +0100

    chore: add pdf button dialog

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
+import DashBoardComponent from "@/components/DashBoard";
 
-const DashBoard = () => {
+const DashBoard = async () => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
@@ -10,7 +12,11 @@ const DashBoard = () => {
     redirect("/auth-callback?orgin=dashboard");
   }
 
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({ where: { id: user.id } });
+
+  if (!dbUser) redirect("/auth-callback?orgin=dashboard");
+
+  return <DashBoardComponent />;
 };
 
 export default DashBoard;
---
+++ components/UploadButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+      <DialogContent>Example</DialogContent>
+    </Dialog>
+  );
+};
+
+export default UploadButton;
