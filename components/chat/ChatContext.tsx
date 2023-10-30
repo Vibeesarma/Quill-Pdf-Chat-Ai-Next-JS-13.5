826551568923fd153e0e9ddd51af5f1533f10ad7
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-10-30 11:50:41 +0000

    chore: create message table and react context for global data pass

---
+++ components/chat/ChatContext.tsx
@@ -0,0 +1,69 @@
+import React, { ReactNode, createContext, useState } from "react";
+import { useToast } from "../ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
+
+type streamResponse = {
+  addMessage: () => void;
+  message: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  isLoading: boolean;
+};
+
+type ChatContextProviderProps = {
+  fileId: string;
+  children: ReactNode;
+};
+
+const ChatContext = createContext<streamResponse>({
+  addMessage: () => {},
+  message: "",
+  handleInputChange: () => {},
+  isLoading: false,
+});
+
+const ChatContextProvider = ({
+  fileId,
+  children,
+}: ChatContextProviderProps) => {
+  const [message, setMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { toast } = useToast();
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed send message");
+      }
+
+      return response.body;
+    },
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const addMessage = () => sendMessage({ message });
+
+  return (
+    <ChatContext.Provider
+      value={{
+        addMessage,
+        message,
+        handleInputChange,
+        isLoading,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+export { ChatContext, ChatContextProvider };
