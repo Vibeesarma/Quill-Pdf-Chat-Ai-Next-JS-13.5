6f0b18df309f9e209d78cbab8f8be9007c5c4802
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-11-05 16:48:45 +0000

    chore: create page for pro plan pricing

---
+++ config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 14,
+      priceIds: {
+        test: "price_1O94k1CF8Ao7QpTJQWsBlhy8",
+        production: "",
+      },
+    },
+  },
+];
