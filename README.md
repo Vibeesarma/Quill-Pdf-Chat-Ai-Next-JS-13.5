f4a2dda5c2df124ced942ea625b3a6b833d3b762
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-11-12 06:05:52 +0000

    feat: add metadata

--- README.md
+++ README.md
@@ -592,6 +592,53 @@ useEffect(() => {
 
 ```
 
+## Seo
+
+- you can create metadata for SEO like below,
+
+```typescript
+export function constructMetadata({
+  title = "Quill- the saas for students",
+  description = "Quill is an open-source software to make chattting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      // put your twitter name its show when some share it this to twitter
+      creator: "",
+    },
+    icons,
+    metadataBase: new URL(process.env.DEPLOY_BASE_URL!),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
+```
+
 ## CSS
 
 - grow - Use grow to allow a flex item to grow to fill any available space.
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,45 @@ export function absoluteUrl(path: string) {
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
 
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = "Quill- the saas for students",
+  description = "Quill is an open-source software to make chattting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      // put your twitter name its show when some share it this to twitter
+      creator: "",
+    },
+    icons,
+    metadataBase: new URL(process.env.DEPLOY_BASE_URL!),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
