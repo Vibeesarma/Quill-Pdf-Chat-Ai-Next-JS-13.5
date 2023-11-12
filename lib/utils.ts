5721da971817acf18109861011fba7ec0d393c86
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-11-12 08:46:16 +0000

    chore: change vercel url to base url

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,7 +9,8 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
 
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  if (process.env.DEPLOY_BASE_URL)
+    return `${process.env.DEPLOY_BASE_URL}${path}`;
 
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
